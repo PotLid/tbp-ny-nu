4e2f881158c319ec5f029b26e54c92a5b21363ac
Jun <hello@junwoo.dev>
2023-03-01 15:37:13 -0500

    update transition

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,12 +3,12 @@ import {NavBar, Return} from "@/components/nav";
 export function SubLayout({children, title}: {children: React.ReactNode, title?: string | null}) {
     return (
         <motion.div
-            initial={{ transform: "scale(1.2)", opacity: 0 }}
-            animate={{ transform: "scale(1)", opacity: 1 }}
-            exit={{ transform: "scale(1.2)", opacity: 0 }}
+            initial={{ transform: "translateY(.25rem)", opacity: 0 }}
+            animate={{ transform: "translateY(0)", opacity: 1 }}
+            exit={{ transform: "translateY(.25rem)", opacity: 0 }}
             transition={{
                 type: "spring",
-                stiffness: 320,
+                stiffness: 325,
                 damping: 40,
             }}
         >
@@ -18,15 +18,15 @@ export function SubLayout({children, title}: {children: React.ReactNode, title?:
     )
 }
 
-export function MainLayout({children, title}: {children: React.ReactNode, title?: string | null}) {
+export function MainLayout({children, title}: {children: React.ReactNode, title?: string}) {
     return (
         <motion.div
-            initial={{ filter: "blur(10px)", opacity: 0 }}
-            animate={{ filter: "blur(0)", opacity: 1 }}
-            exit={{ filter: "blur(10px)", opacity: 0 }}
+            initial={{ filter: "blur(.1rem)",transform: "scale(.95)", opacity: 0 }}
+            animate={{ filter: "blur(0)", transform: "scale(1)", opacity: 1 }}
+            exit={{ filter: "blur(.1rem)",transform: "scale(.95)", opacity: 0 }}
             transition={{
                     type: "spring",
-                    stiffness: 220,
+                    stiffness: 325,
                     damping: 40,
                 }}
         >
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import {AnimatePresence} from "framer-motion";
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
-      <main className="flex flex-col">
+      <main className="flex flex-col w-[100vh] h-[100vh]">
           <AnimatePresence mode="wait" initial={false} >
               <Component {...pageProps} key={router.asPath}/>
           </AnimatePresence>
