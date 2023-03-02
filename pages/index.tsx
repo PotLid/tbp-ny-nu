1abedddd818a348f6bc640c3a1a81de476e38ce2
PotLid <JunwooDev@gmail.com>
2023-03-02 02:12:45 -0500

    update design

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import tbpLogo from './logo-bg.png';
 export default function Home( {isLoaded} : {isLoaded?: boolean} ) {
     return (
         <MainLayout className="relative z-10">
-            <header className="relative flex flex-col items-center max-w-full text-center" style={{backgroundImage: "radial-gradient(rgba(0,0,0,0.25) 25%, rgba(0,0,0,0) 55%)"}}>
+            <header className="relative flex flex-col items-center max-w-full text-center landing-gradient" style={{backgroundImage: "radial-gradient(rgba(0,0,0,0.25) 25%, rgba(0,0,0,0) 55%)"}}>
                 <div className="w-[5.5rem] h-[5.5rem] leading-[5.5rem] order border-white rounded-full">
                     <Image src={tbpLogo} alt="Tau Beta Pi Logo" />
                 </div>
