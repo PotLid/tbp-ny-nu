fe625656cf24e5595955853a62c6c56507362372
PotLid <JunwooDev@gmail.com>
2023-03-02 00:47:21 -0500

    backgroud delay

--- components/Background/index.tsx
+++ components/Background/index.tsx
@@ -1,8 +1,18 @@
 import {BackgroundComponent} from "./types";
+import {motion} from "framer-motion";
 import styles from "./styles.module.css";
 
 export const Background: BackgroundComponent = ({isMain, className, isLoaded}) => {
     return (
-        <div className={`${isLoaded ? "" : styles["bg-loading"]} ${styles.background} ${isMain ? "" : styles.active}`} />
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+                type: "ease-in-out",
+                duration: .325,
+                delay: .625
+            }}
+            className={`${styles.background} ${isMain ? "" : styles.active}`}
+        />
     )
 }
