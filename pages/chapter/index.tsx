56351a473d32f52e81080739a7d8f8dc02dacec4
PotLid <JunwooDev@gmail.com>
2023-03-02 03:05:08 -0500

    udate bold

--- pages/chapter/index.tsx
+++ pages/chapter/index.tsx
@@ -21,16 +21,16 @@ export default function Chapter() {
                         <b>Tau Beta Pi</b> is universally recognized as the preeminent engineering honor society that students of all engineering disciplines strive to join.
                     </p>
                     <p className="mx-0 mb-4 mt-0">
-                        It is the nation&#39;s <b>second</b> oldest honor society, founded at Lehigh University in 1885. It is the only engineering honor society that recognizes individuals from all engineering disciplines. There are currently <b>241</b> chapters at colleges and universities across the U.S., <b>32</b> active alumni chapters across <b>15</b> districts, and a total initiated membership of over half a million. Tau Beta Pi&#39;s vision is to recognize, celebrate, and champion excellence in engineering.
+                        It is the nation&#39;s <b>second oldest</b> honor society, founded at Lehigh University in 1885. It is the only engineering honor society that recognizes individuals from all engineering disciplines. There are currently <b>241 chapters</b> at colleges and universities across the U.S., 32 active alumni chapters across 15 districts, and a total initiated membership of over half a million. Tau Beta Pi&#39;s vision is to recognize, celebrate, and champion excellence in engineering.
                     </p>
                     <p className="mx-0 mb-4 mt-0">
                         <b>The New York Nu chapter</b> at the University at Buffalo, established in 1967, organizes a wide variety of events for club members and the campus community. Most notable and long lived are our Honors Dinner and engineering tutoring programs.
                     </p>
                     <p className="mx-0 mb-4 mt-0">
-                        Since it&#39;s inauguration <b>485,337</b> members have been inducted into Tau Beta Pi. Currently there are <b>230</b> active collegiate chapter in the United States. For more information on the national organization please visit <a className="custom-anchor" href="https://www.tbp.org" rel="noreferrer noopener" target="_blank">www.tbp.org</a>.
+                        Since it&#39;s inauguration, <b>485,337 members</b> have been inducted into Tau Beta Pi. Currently there are <b>230 active collegiate chapters</b> in the United States. For more information on the national organization please visit <a className="custom-anchor" href="https://www.tbp.org" rel="noreferrer noopener" target="_blank">www.tbp.org</a>.
                     </p>
                     <p className="mx-0 mb-4 mt-0">
-                        The New York Nu Chapter has a total of <b>3,713</b> members.
+                        The New York Nu Chapter has a total of <b>3,713 members</b>.
                     </p>
                 </article>
                 <Close />
